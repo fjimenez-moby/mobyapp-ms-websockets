ffb581196b033964fab6ab71b34a6a69be37c2a6
Santiago Gallardo <sgallardo3@bitbucket.org>
2025-11-12 12:44:31 -0300

    Agregar configuración de Eureka Discovery y FeignClient para que reconozca el microservicio el API Gateway

--- src/main/java/com/mobydigital/mobyapp/websocket/WebsocketApplication.java
+++ src/main/java/com/mobydigital/mobyapp/websocket/WebsocketApplication.java
@@ -2,8 +2,12 @@ package com.mobydigital.mobyapp.websocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class WebsocketApplication {
 
 	public static void main(String[] args) {
